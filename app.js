2b0abfaa9c3cd60dcfc9b46e781cc8a281b94f49
Jairus Omollo <omollojairus@gmail.com>
2020-11-14 17:26:52 +0300

    removed --dirname on public folder

--- app.js
+++ app.js
@@ -12,20 +12,20 @@ const session = require('express-session')
 const MongoStore = require ('connect-mongo')(session)
 const { check, validationResult } = require('express-validator');
 const compression = require('compression')
+process.env.PWD = process.cwd()
 
-//start express app
+    //start express app
 const app = express();
-if(process.env.NODE_ENV === 'production'){
 
 //register view engine
 app.set('view engine', 'ejs');
 
 //middleware and static files
 app.use(cors())
-app.use(express.static(__dirname +'/public/css'));
-app.use(express.static(__dirname +'/public/js'));
-app.use(express.static(__dirname +'/public/img'));
-app.use(express.static(__dirname +'/public/google-address-autocomplete'));
+app.use(express.static( process.env.PWD + '/public/css' ));
+app.use(express.static(process.env.PWD + '/public/js'));
+app.use(express.static( process.env.PWD +'/public/img'));
+app.use(express.static(process.env.PWD + '/public/google-address-autocomplete'));
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -55,4 +55,4 @@ app.use('/api/v1/home', budgetRouter )
 
 
 module.exports = app;
-}
+
